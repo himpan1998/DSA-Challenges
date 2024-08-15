47a879c3d68fe83a3abd70d90ae4946272052247
Himanshu <himanshupandey2016@gmail.com>
2024-08-15 18:47:43 +0530

    find-duplicate-elements in an array

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const {
   findDuplicate,
   findRepeatedArray,
   findRepeatedArray1,
-} = require("./problems/basic-problems/array-problems/array-duplicate");
+} = require("./problems/basic-problems/array-problems/find-duplicate(repeated)-element");
 // Execute the problems
 const {
   findUniqueElements,
