2b66646fe2715946b24ba32490920c1b731d1f4f
himanshu.pandey <himanshu.pandey@shyamsteel.com>
2024-12-07 09:57:09 +0530

    add two

---
+++ problems/clousers.js
@@ -0,0 +1,15 @@
+function createCounter() {
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const counter = createCounter();
+console.log(counter()); // Output: 1
+console.log(counter()); // Output: 2
+console.log(counter()); // Output: 3
+
+module.exports = { createCounter };
