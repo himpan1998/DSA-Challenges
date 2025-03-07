a9c4a8ee9faaca15d9673c58f64a03b78f6f90c9
himanshu.pandey <himanshu.pandey@shyamsteel.com>
2025-03-07 07:46:26 +0530

    dsa array

--- problems/basic-problems/array-problems/array.js
+++ problems/basic-problems/array-problems/array.js
@@ -4,12 +4,53 @@ const cars = new Array("BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi");
 
 // at
 let car = cars.at(0);
-console.log("car:", car);
+// console.log("car:", car);
 
 // concat
 let cars2 = cars.concat("Honda");
-console.log("cars2:", cars2);
+// console.log("cars2:", cars2);
 
 const arr = [1, 23, 4, 5, 6, 7, 8, 9, 10];
 let arr1 = arr.concat(11, 12, 13, 14, 15);
-console.log(arr1);
+console.log("aaa");
+// console.log(arr1);
+
+// copyWithin
+// const arr2 = [1, 2, 3, 4, 5];
+// let arr3 = arr2.copyWithin(0, 3);
+// console.log(arr3);
+// construtor:
+
+// copyWithin
+// entries
+// every
+// fill
+// filter
+// find
+// findIndex
+// flat
+// flatMap
+// forEach
+// from
+// includes
+// indexOf
+// join
+// keys
+// lastIndexOf
+// length
+// map
+// pop
+// push
+// reduce
+// reduceRight
+// reverse
+// shift
+// slice
+// some
+// sort
+// splice
+// toLocaleString
+// toString
+// unshift
+// values
+// Symbol.iterator
