10c4921a16f91eacde3b2862562649f7418d2d33
himanshu.pandey <himanshu.pandey@shyamsteel.com>
2025-06-02 08:22:34 +0530

    a

--- problems/basic-problems/number-problems/BC-1/buildisthan/backend/product-service/middleware/extractUserInfo.ts
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-const extractUserInfo = (req : any, res : any, next : any) => {
-    const result : any = req.headers['authorization'] ? jwt.decode(req.headers['authorization'].split(' ')[1]) : ''
-    req.user_id = result?.id || 0;
-    next();
-}
-
-export default extractUserInfo
