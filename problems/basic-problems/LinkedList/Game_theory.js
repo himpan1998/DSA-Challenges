8bf1eecfbfc80eabe37147a518bb5a7c6d6ba8dc
HIMANSHU PANDEY <100412097+himpan1998@users.noreply.github.com>
2025-06-04 09:21:58 +0530

    Update Game_theory.js

--- problems/basic-problems/LinkedList/Game_theory.js
+++ problems/basic-problems/LinkedList/Game_theory.js
@@ -18,6 +18,22 @@
  * 
  * 
 
+Subject: Resignation Letter – Pursuing Higher Studies
+
+Dear Sir,
+
+I am writing to formally resign from my position as Management Trainee at Shyam Steel, effective 5th July 2025.
+
+This decision comes after careful consideration, as I have recently been offered the opportunity to pursue higher studies to further advance my academic and professional goals. While I am excited about this next chapter, it was not an easy decision to make, given the enriching experiences and valuable learning I have gained during my time at Shyam Steel.
+
+Working under your guidance and alongside such a talented team has significantly contributed to my personal and professional development. I am truly grateful for the support, exposure, and opportunities I’ve had here, and I carry forward many positive learnings into my future endeavors.
+
+I will ensure a smooth transition during my notice period and am happy to assist in handing over my responsibilities to the designated team member.
+
+Thank you once again for the support and encouragement throughout my tenure. I hope to stay in touch, and I look forward to crossing paths again in the future.
+
+Warm regards,
+Himanshu Pandey
 
 
 
